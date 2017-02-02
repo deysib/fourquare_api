3bc04cedb22f9e339c1267dce70809d61171334a
deysib <dbautista@cristoreyny.org>
2017-02-02 13:43:41 +0000

    my comment

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 /*global$*/
 $(document).ready(function(){
-    $("#Search").click(function() {
-        var type=  $("input").val();
-        var near= $("input").val();
+    $("#git puSearch").click(function() {
+        var type=  $("#Search").val();
+        var near= $("#Zipcode").val();
         var url= "https://api.foursquare.com/v2/venues/search?client_id=GJP350DH3G2PX544RXXXML0B22PC32D4XYDTN5OGIH2X0HTE&client_secret=H0MTUBOXYGXFS1OJHRRAT23MCDL5KZKGJGHXK15X1BZZR4UC&v=20130815&";
         var url1="&ll=";
         var url2="&query=";
@@ -10,8 +10,8 @@ $(document).ready(function(){
 
         $.getJSON(url11, function(response){
              $("#Results").append("Here are your results"+"<br>" + response.response.venues[0].name + "<br>"); 
-             $("#Results").append("Here are your results"+"<br>" + response.response.venues[0].phone + "<br>"); 
-             $("#Results").append("Here are your results"+"<br>" + response.response.venues[0].location.address + "<br>"); 
+             $("#Results").append("Phone Number"+"<br>" + response.response.venues[0].phone + "<br>"); 
+             $("#Results").append("Address"+"<br>" + response.response.venues[0].location.address + "<br>"); 
       
             
         
